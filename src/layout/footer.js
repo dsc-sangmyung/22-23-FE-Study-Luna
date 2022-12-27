7b2ac1e3fe44509a3974b1d3188e3f7f9fcf36aa
YeonAh Kim <yuna@YeonAhui-MacBookPro.local>
2022-12-27 21:07:00 +0900

    clone start

---
+++ src/layout/footer.js
@@ -0,0 +1,11 @@
+import './footer.css'
+
+const Footer = () => {
+    return (
+        <footer className='footer'>
+            
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/layout/layout.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./header";
+import "./layout.css"
+
+const Layout = () => {
+    return (
+        <div className="layout">
+            <Header />
+            <main className="main">
+
+            </main>
+            <Footer />
+        </div>
+    )
+}
+
+export default Layout
